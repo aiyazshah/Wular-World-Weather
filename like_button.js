83b0e58db78db48dad2b2bddcdbd6251437939c4
Aiyaz gani <shahaiyaz71@gmail.com>
2022-03-09 17:59:40 +0530

    test

---
+++ like_button.js
@@ -0,0 +1,11 @@
+
+
+
+
+// ReactDOM.render(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('root')
+// );
+let x=document.getElementById("root");
+ReactDOM.render(<h1 type="text/babel">hello wro</h1>,x);
+
